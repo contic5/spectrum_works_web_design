1cf4281a7654c8aa9284c16bcc76d92fd49bdc25
Christopher Joseph Conti <36709920+contic5@users.noreply.github.com>
2022-12-27 17:06:45 -0500

    Add files via upload

---
+++ Project Solutions/A Side/Project8MabLibs/madlib.js
@@ -0,0 +1,18 @@
+let source="https://www.madlibs.com/wp-content/uploads/2016/04/VacationFun_ML_2009_pg15.pdf";
+
+let results=`A vacation is when you take a trip to some ${adj1} place
+with your ${adj2} family. Usually you go to some place
+that is near a/an ${noun1} or up on a/an ${noun2}.
+A good vacation place is one where you can ride ${pluralnoun1}
+or play ${game} or go hunting for ${pluralnoun2} . I like
+to spend my time ${ingverb1} or ${ingverb2}.
+When parents go on a vacation, they spend their time eating
+three ${pluralnoun3} a day, and fathers play golf, and mothers
+sit around ${ingverb3}. Last summer, my little brother
+fell in a/an ${noun2} and got poison ${plant} all
+over his ${partofbody} My family is going to go to (the)
+${place}, and I will practice ${ingverb4}. Parents
+need vacations more than kids because parents are always very
+${adj3} and because they have to work ${number}
+hours every day all year making enough ${pluralnoun4} to pay
+for the vacation.`;
---
+++ Project Solutions/A Side/Project8MabLibs/words.js
@@ -0,0 +1,7 @@
+let skill="jogging";
+let place="FM 3: The Revenge";
+let adjective="alarming";
+let number=31870;
+let adjective2="invincible";
+let time="decade";
+let skill2="climbing";
